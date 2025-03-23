77de291b955175ebc12f9dee3370a6c8df57f5a2
Cyril447 <cyril.bouhtou@gmail.com>
2025-03-23 20:40:13 +0100

    Create LienC#SQL.cs

---
+++ LienC#SQL.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.VisualBasic.FileIO;
+using MySql.Data.MySqlClient;
+
+namespace LienC_SQL
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
